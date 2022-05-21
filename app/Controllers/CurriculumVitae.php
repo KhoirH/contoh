ab283567f38deeb5b5dab0b6c2ae5cc646a8eedf
KhoirH <hilmi.khoirulloh02@gmail.com>
2022-05-22 00:33:58 +0700

    added migration and seeder

---
+++ app/Controllers/CurriculumVitae.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\I18n\Time;
+
+
+class CurriculumVitae extends BaseController
+{
+    public function index()
+    {
+        return view('CurriculumVitaeView');
+    }
+
+
+}
--- app/Views/CurriculumVitaeView.php
+++ app/Views/CurriculumVitaeView.php
@@ -3,10 +3,7 @@
 <?= $this->section('content') ?>
     <!-- Page Wrapper -->
     <div id="wrapper">
-
-        <?= $this->include('component/Sidebar') ?>
-
-
+      <?= $this->include('component/Sidebar') ?>
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
 
@@ -15,18 +12,10 @@
 
                 <?= $this->include('layout/topBar') ?>
 
-
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-                    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-                        For more information about DataTables, please visit the <a target="_blank"
-                            href="https://datatables.net">official DataTables documentation</a>.</p>
-
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
+                   <!-- DataTales Example -->
+                   <div class="card shadow mb-4">
                         <div class="card-header py-3">
                             <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
                         </div>
@@ -514,8 +503,7 @@
                                 </table>
                             </div>
                         </div>
-                    </div>
-
+                    </div> 
                 </div>
                 <!-- /.container-fluid -->
 
@@ -526,7 +514,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
+                        <span>Copyright &copy; Your Website 2021</span>
                     </div>
                 </div>
             </footer>
@@ -537,30 +525,5 @@
 
     </div>
     <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="<?php echo base_url().'/login' ?>">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
 
 <?= $this->endSection() ?>
